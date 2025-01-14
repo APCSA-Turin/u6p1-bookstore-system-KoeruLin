64afe2accaeed007850e4d9cbd047a9b2606aeec
KoeruLin <167535514+KoeruLin@users.noreply.github.com>
2025-01-14 14:42:57 +0000

    in progress

--- src/main/java/com/example/project/BookStore.java
+++ src/main/java/com/example/project/BookStore.java
@@ -1,31 +1,146 @@
 package com.example.project;
 
-public class BookStore{
+public class BookStore
+{
+    private Book[] books = new Book[0];
+    private User[] users = new User[10];
+
+    public BookStore()
+    {
+    }
 
     //requires at least 2 attributes Book[] books, User[] users (initialized to an empty array of 10 max users) 
 
     //requires 1 empty constructor
 
-    // public getUsers(){}
+    public User[] getUsers()
+    {
+        return users;
+    }
 
-    // public setUsers(){}
+    public void setUsers(User[] newUsers)
+    {
+        users = newUsers;
+    }
 
-    // public  getBooks(){}
+    public Book[] getBooks()
+    {
+        return books;
+    }
 
-    // public void addUser(User user){} 
+    public void addUser(User user)
+    {
+        for(int i = 0; i < users.length; i++)
+        {
+            if(users[i] == null)
+            {
+                users[i] = user;
+                break;
+            }
+        }
+    } 
 
-    // public void removeUser(User user){}
+    public void removeUser(User user)
+    {
+        for(int i = 0; i < users.length; i++)
+        {
+            if(users[i] == user)
+            {
+                users[i] = null;
+                break;
+            }
+        }
+        
+        consolidateUsers();
 
-    // public void consolidateUsers(){}
+        Book[] newBook = new Book[books.length - 1];
 
-    // public void addBook(Book book){}
+        for(int i = 0; i < newBook.length; i++)
+        {
+            newBook[i] = books[i];
+        }
+        
+        books = newBook;
+    }
+    // wrong, that user index need to be completely removed
 
-    // public void insertBook(Book book, int index){}
+    public void consolidateUsers()
+    {
+        User[] newUser = new User[users.length];
+        int i = 0;
 
-    // public void removeBook(Book book){}
-       
-    // public String bookStoreBookInfo(){} //you are not tested on this method but use it for debugging purposes
+        for (User user : users) 
+        {
+            if (user != null) 
+            {
+                newUser[i] = user; 
+                i++;
+            }
+        }
 
-    // public String bookStoreUserInfo(){} //you are not tested on this method but use it for debugging purposes
+        users = newUser;  
+    }
 
+    public void addBook(Book book)
+    {
+        Book[] newBook = new Book[books.length + 1];
+        for(int i = 0; i < books.length; i++)
+        {
+            newBook[i] = books[i];
+        }
+
+        if (books.length == 0)
+        {
+            newBook[0] = book;
+        }
+        else
+        {
+            newBook[newBook.length - 1] = book;
+        }
+
+        books = newBook;
+    }
+
+    public void insertBook(Book book, int index)
+    {
+        if(books[index] == null)
+        {
+            books[index] = book;
+        }
+        else
+        {
+            Book[] newBook = new Book[books.length + 1];
+
+            for(int i = 0; i < index + 1; i++)
+            {
+                newBook[i] = books[i];
+            }
+            
+            newBook[index] = book;
+
+            for(int z = index + 1; z < books.length; z++)
+            {
+                newBook[z] = books[z - 1];
+            }
+            
+            books = newBook;
+        }
+    }
+
+    public void removeBook(Book book)
+    {
+        for(int i = 0; i < books.length; i++)
+        {
+            if(books[i] == book)
+            {
+                books[i] = null;
+                break;
+            }
+        }
+    }
+
+    public String bookStoreUserInfo()
+    {
+        return "";
+    } //you are not tested on this method but use it for debugging purposes
 }
--- src/main/java/com/example/project/User.java
+++ src/main/java/com/example/project/User.java
@@ -1,24 +1,66 @@
 package com.example.project;
 
-public class User{
+public class User
+{
     //requires 3 private attributes String name, String Id, Book book that is initialized to empty
 
+    private String name;
+    private String Id;
+    private Book[] book;
+
     //requires 1 contructor with two parameters that will initialize the name and id
+
+    public User(String name, String id)
+    {
+        this.name = name;
+        Id = id;
+    }
  
-    // public  getName() {}
+    public String getName() 
+    {
+        return name;
+    }
 
-    // public  setName() {}
+    public void setName(String newName) 
+    {
+        name = newName;
+    }
 
-    // public  getId() {}
+    public String getId() 
+    {
+        return Id;
+    }
 
-    // public void setId() {}
+    public void setId(String newId) 
+    {
+        Id = newId;
+    }
 
-    // public getBooks() {}
+    public Book[] getBooks() 
+    {
+        return book;
+    }
 
-    // public setBooks() {}
+    public void setBooks(Book[] newBook) 
+    {
+        book = newBook;
+    }
 
-    // public String bookListInfo(){} //returns a booklist for the user, if empty, output "empty"
+    public String bookListInfo()
+    {
+        String info = "";
 
-    // public String userInfo(){} //returns  "Name: []\nID: []\nBooks:\n[]"
-       
+        for (int i = 0; i > book.length; i++)
+        {
+            info += (book[i] == null) ? "empty\n" : book[i].getTitle() + "\n"; 
+        }
+
+        return info;
+    } //returns a booklist for the user, if empty, output "empty"
+
+    public String userInfo()
+    {
+        return  "Name: " + name + "\n" + "ID: " + IdGenerate.generateID() + "\n" + "Books:\n" + bookListInfo();
+    } 
+    //returns  "Name: []\nID: []\nBooks:\n[]"   
 }
